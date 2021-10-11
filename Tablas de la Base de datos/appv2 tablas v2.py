d73dad0c4a8fc28eab3d72eed292a0e0efac5233
Jona2010 <38143615+Jona2010@users.noreply.github.com>
2021-10-11 12:59:51 -0500

    Update appv2 tablas v2.py

--- Tablas de la Base de datos/appv2 tablas v2.py
+++ Tablas de la Base de datos/appv2 tablas v2.py
@@ -4,15 +4,7 @@ from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-#libraries: pip3 install flask-sqlalchemy flask-marshmallow marshmallow-sqlalchemy pymysql
-
 app = Flask(__name__)
-
-# para que postgresql funcione
-#sudo pip3 install psycopg2-binary
-
-# mysql
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:290519@127.0.0.1:3306/flaskmysql'
 
 #posgresql
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:290519@127.0.0.1:8080/proyectocs"
@@ -69,53 +61,6 @@ class TaskSchema(ma.Schema):
 
 task_schema = TaskSchema()
 tasks_schema = TaskSchema(many=True)
-
-@app.route('/create_task', methods=['POST'])
-def create_task():
-    print(request.json)
-
-    title = request.json['title']
-    description = request.json['description']
-
-    new_task = Task(title, description)
-    db.session.add(new_task)
-    db.session.commit()
-
-    return task_schema.jsonify(new_task) # respondemos cpon la tarea creada
-
-@app.route('/tasks', methods=['POST'])
-def tasks():
-    all_tasks = Task.query.all()
-    result = tasks_schema.dump(all_tasks) # lo vuelve serializable
-    return jsonify(result)
-
-
-@app.route('/task/<id>', methods=['POST'])
-def task(id):
-    result = Task.query.get(id)
-    result = task_schema.dump(result) # lo vuelve serializable
-    return jsonify(result)
-
-@app.route('/update_task/<id>', methods=['POST'])
-def update_task(id):
-    task = Task.query.get(id)
-    title = request.json['title']
-    description = request.json['description']
-
-    task.title = title
-    task.description = description
-
-    db.session.commit()
-    return task_schema.jsonify(task)
-
-
-@app.route('/delete_task/<id>', methods=['POST'])
-def delete_task(id):
-    task = Task.query.get(id)
-    db.session.delete(task)
-    db.session.commit()
-    return task_schema.jsonify(task)
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
